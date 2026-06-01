935fa5e5bc78e6164307bba1b93a2a056d8aff37
babarzonecom-web <babarzone.com@gmail.com>
2026-06-01 22:30:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,9 @@
-// v469.8-auto-update
-const SW_VERSION = 'v469.8';
+// v469.9
+const SW_VERSION = 'v469.9';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-      .then(() => self.clients.matchAll({ type: 'window' }))
-      .then(clients => clients.forEach(c => { c.postMessage({ type: 'SW_UPDATED', version: SW_VERSION }); c.navigate(c.url); }))
-  );
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()).then(()=>self.clients.matchAll({type:'window'})).then(clients=>clients.forEach(c=>{c.postMessage({type:'SW_UPDATED',version:SW_VERSION});c.navigate(c.url);})));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request, { cache: 'no-store' }).catch(() => caches.match(e.request)));
+  e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
 });

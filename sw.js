02fe5ce6f940f0ff4e7fbf2e81fa457e77383909
babarzonecom-web <babarzone.com@gmail.com>
2026-06-01 22:45:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,4 @@
-// v469.9
-const SW_VERSION = 'v469.9';
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim()).then(()=>self.clients.matchAll({type:'window'})).then(clients=>clients.forEach(c=>{c.postMessage({type:'SW_UPDATED',version:SW_VERSION});c.navigate(c.url);})));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));
-});
+const SW_VERSION='v469.10';
+self.addEventListener('install',e=>{self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.map(c=>caches.delete(c)))).then(()=>self.clients.claim()).then(()=>self.clients.matchAll({type:'window'})).then(cs=>cs.forEach(c=>{c.postMessage({type:'SW_UPDATED',version:SW_VERSION});c.navigate(c.url);})));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)));});
